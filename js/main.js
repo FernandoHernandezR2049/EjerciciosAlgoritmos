36c6529b225a8eef7642468e578acebb26f25517
Fernando Hernandez <fernandohruz@hotmail.com>
2022-05-25 16:47:55 -0500

    Primer commit de los primeros ejercicios de Javascript

---
+++ js/main.js
@@ -0,0 +1,74 @@
+function askFor3UserInputs() {
+    const num1= parseFloat(prompt("Introduce el primer número"));
+    const num2= parseFloat(prompt("Introduce el segundo número"));
+    const num3= parseFloat(prompt("Introduce el tercer número"));
+    return [num1, num2, num3];
+}
+function max(){
+    [num1,num2, num3] = askFor3UserInputs();   
+    let message = "";
+    if((num1 > 100 || num1 < 0)||(num2 > 100 || num2 < 0)||(num3 > 100 || num3 < 0))
+        alert("Alguno de los números se encuentran fuera del rango solictado")
+    else{
+        if(num1>=num2 && num1>=num3)
+            message = `${num1} es el número mayor`;
+        else if(num2>=num1 && num2>=num3)
+            message = `${num2} es el número mayor`;
+        else
+            message = `${num3} es el número mayor`;
+        alert(message);
+    }
+    
+}
+function min(){
+    [num1,num2, num3] = askFor3UserInputs();
+    let message = "";
+    if((num1 > 100 || num1 < 0)||(num2 > 100 || num2 < 0)||(num3 > 100 || num3 < 0))
+        alert("Alguno de los números se encuentran fuera del rango solictado")
+    else{
+        if(num1<=num2 && num1<=num3)
+            message = `${num1} es el número menor`;
+        else if(num2<=num1 && num2<=num3)
+            message = `${num2} es el número menor`;
+        else
+            message = `${num3} es el número menor`;
+        alert(message);
+    }
+    
+}
+function isMultipleOf3() {
+    const num = parseInt(prompt("Introduce un entero entre el 100 y el 200"));
+    let message = "";
+    if(num>=100 || num<=200)
+        message = "El número introducido no cumple con las caracteristicas especificadas";
+    else{
+        if(num%3 === 0)
+            message = "Tu número es múltiplo de 3";
+        else
+            message = "Tu número no es múltiplo de 3";
+    }
+    
+    alert(message);
+
+}
+function isTheSumOfTheOthers(){
+    [num1, num2, num3] = askFor3UserInputs();
+    if (num1 === num2 + num3)
+        message =  `El número ${num1} es la suma de los otros dos`;
+    else if(num2 === num1 + num3)
+        message = `El número ${num2} es la suma de los otros dos`;
+    else if(num3 === num1+ num2)
+        message = `El número ${num3} es la suma de los otros dos`;
+    alert(message);
+}
+function isEven(){
+    const num = prompt("Introduce un número para ver si es par o impar");
+    let message = "";
+    if (num%2 === 0)
+        message = `${num} es par`;
+    else if (num%2 === 1 || num%2 === -1)
+        message = `${num} es impar`;
+    else
+        message = "No se brou, seguro pusiste un decimal o una palabra";
+    alert(message)
+}
